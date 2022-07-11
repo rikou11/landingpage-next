f9f6008454fd51c7533855b38b5c99cf018806fe
fares <fares.bensaad@univ-constantine2.dz>
2022-07-12 02:25:12 +0400

    comit

--- components/Footer.js
+++ components/Footer.js
@@ -89,7 +89,7 @@ const Footer = () => {
         <div class="bg-gray-100">
           <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
             <p class="text-gray-500 text-sm text-center sm:text-left">
-              © 2020 bensaad fares —
+              © 2022 bensaad fares —
               <a
                 href="https://twitter.com/knyttneve"
                 rel="noopener noreferrer"
